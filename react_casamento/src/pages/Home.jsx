e9a5aada2f3d88f554c9b6f53da62611fd76c25f
Orlando Vieira Neto <vieiranetoorlando@gmail.com>
2024-08-24 01:44:21 -0300

    organizando

---
+++ react_casamento/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Bem-vindo ao nosso site de casamento!</h2>
+      <p>Estamos muito felizes em compartilhar esse momento especial com você.</p>
+    </div>
+  );
+}
+
+export default Home;
